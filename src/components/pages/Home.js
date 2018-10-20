e3250ce85ff38d043fe31e4541452435eee7229b
Akshay Gulhane <akshay@proxce.com>
2018-10-20 16:15:15 +0530

    Changes for secure routes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withAuth } from "@okta/okta-react";
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
-}
+export default withAuth(
+  class Home extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { authenticated: null };
+    }
+
+    checkAuthentication = async () => {
+      const authenticated = await this.props.auth.isAuthenticated();
+      if (authenticated !== this.state.authenticated) {
+        this.setState({ authenticated });
+      }
+    };
+
+    async componentDidMount() {
+      this.checkAuthentication();
+    }
+
+    async componentDidUpdate() {
+      this.checkAuthentication();
+    }
+
+    login = async () => {
+      this.props.auth.login("/");
+    };
+
+    logout = async () => {
+      this.props.auth.logout("/");
+    };
+
+    render() {
+      if (this.state.authenticated === null) return null;
+
+      const mainContent = this.state.authenticated ? (
+        <div>
+          <p className="lead">
+            {" "}
+            You have entered the Staff Portal,{" "}
+            <Link to="/staff"> Click here </Link>
+          </p>
+        </div>
+      ) : (
+        <div />
+      );
+
+      return (
+        <div className="jumbotron">
+          <h1 className="display-4">Proxce Dev Portal</h1>
+        </div>
+      );
+    }
+  }
+);
